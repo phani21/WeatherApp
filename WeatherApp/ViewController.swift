1df93eae01710c2c924ac46cb405d8496c2b5d0c
Phani_Kiran <pret321@gmail.com>
2019-02-21 17:01:10 -0600

    commit

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,37 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-   
+var res = ""
+class ViewController: UIViewController {
+    
+    
     @IBOutlet weak var outputField: UITextView!
     @IBOutlet weak var cityname: UITextField!
-   
+    
     @IBAction func submitButton(_ sender: Any) {
-   
-    let stringToShow = cityname.text
-    let newStringToShow = stringToShow?.replacingOccurrences(of: " ", with: "-")
-    let urlString = "https://www.weather-forecast.com/locations/" + newStringToShow! + "/forecasts/latest"
-   // let search = "p class=\"b-forecast__table-description-content\">"
-    let url = URL(string: urlString)
-    
-    do {
-    let contents = try String(contentsOf: url!)
-    
-    } catch {
-    outputField.text="Please enter a valid City name! "
-    outputField.backgroundColor = .white
-    outputField.textColor = .red
-    }
-   
         
+        let stringToShow = cityname.text
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + stringToShow! + "&appid=4e50d2279e5b4d7501d1092f83ea5345"
+        // https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4e50d2279e5b4d7501d1092f83ea5345
+     
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error == nil {
+                if let unWrappedData = data {
+                    do{
+                        let jsonResult = try JSONSerialization.jsonObject(with: unWrappedData, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+                        
+                        let weather = jsonResult!["weather"] as? NSArray
+                        let weatherItem = weather?[0] as? NSDictionary
+                        let description = weatherItem?["description"] as? String?
+                        if let res = description {
+                           // print(description)
+                           // print(res)
+                            DispatchQueue.main.async {
+                                if( res == nil){
+                                    self.outputField.text="Please enter a valid City name! "
+                                    self.outputField.backgroundColor = .white
+                                    self.outputField.textColor = .red
+                                }
+                                else{
+                                    self.outputField.text = res?.uppercased()
+                                    self.outputField.textColor = .black
+                                }
+                            }
+                        }
+                    }catch{
+                        print("Error fetching API DATA")
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+        }
+        
+        task.resume()
+        print(res)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor(patternImage: UIImage(named: "weather.jpeg")!)
     }
-
-
+    
 }
-
